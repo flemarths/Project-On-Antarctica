ca80ff9e15ae378bbdb2664ca3f09afa7575eb81
flemmarths <arths.sire@gmail.com>
2023-11-07 08:24:46 +0100

    ajout du fichier de l'eval

---
+++ scripts/Evalmatlab.m
@@ -0,0 +1,35 @@
+%EvalMatlab.M : Dernière séance de matlab (évaluée)
+% 
+% 
+% Création le 07/11
+%
+% 
+%
+% SANCHEZ Arthur - Novembre 2023
+ 
+clear all; close all; clc;
+
+
+% %
+% ********** Declaration des constantes ************
+% Dans cette zone sont initialisees TOUTES les constantes necessaires a
+% l'execution du script
+% 
+
+
+
+%%
+%********** Acquisition/Generation des signaux ****
+% Dans cette zone sont declares TOUS les signaux autres que les constantes
+
+
+%%
+%********** Traitement des signaux ****************
+% Dans cette zone sont effectues tous les calculs et traitements des
+% grandeurs etudiees
+
+
+%%
+%********** Visualisation des données *************
+% Cette zone permet de regrouper toutes les instructions relatives au trace
+% des courbes
